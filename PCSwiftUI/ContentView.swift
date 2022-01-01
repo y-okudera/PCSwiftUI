3566b4dc36f1d506fc13d57a9ced23048238d845
yuoku <appledev.yuoku@gmail.com>
2022-01-01 23:54:31 +0900

    Create a new SwiftUI project.

---
+++ PCSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PCSwiftUI
+//
+//  Created by Yuki Okudera on 2022/01/01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PCSwiftUI/PCSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PCSwiftUIApp.swift
+//  PCSwiftUI
+//
+//  Created by Yuki Okudera on 2022/01/01.
+//
+
+import SwiftUI
+
+@main
+struct PCSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
