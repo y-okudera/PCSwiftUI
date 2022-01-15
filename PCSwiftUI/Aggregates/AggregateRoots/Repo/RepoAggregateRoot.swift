d2d2263785c12c99a41571f1c26997c2a7642b4e
yuoku <appledev.yuoku@gmail.com>
2022-01-16 04:07:52 +0900

    Update AggregateRoots to immutable.

--- PCSwiftUI/Aggregates/AggregateRoots/Repo/RepoAggregateRoot.swift
+++ PCSwiftUI/Aggregates/AggregateRoots/Repo/RepoAggregateRoot.swift
@@ -8,39 +8,58 @@
 import Foundation
 
 struct RepoAggregateRoot {
-  private(set) var page: Int
-  private(set) var hasNext: Bool
-  private(set) var repositoryIDs: [String]
-  private(set) var repositoriesByID: [String: RepoAggregate]
-  private(set) var ownersByID: [String: UserAggregate]
+  let page: Int
+  let hasNext: Bool
+  let repositoryIDs: [String]
+  let repositoriesByID: [String: RepoAggregate]
+  let ownersByID: [String: UserAggregate]
 
   var repositories: [RepoAggregate] {
     repositoryIDs.compactMap { repositoriesByID[$0] }
   }
 
+  init(
+    page: Int,
+    hasNext: Bool,
+    repositoryIDs: [String],
+    repositoriesByID: [String: RepoAggregate],
+    ownersByID: [String: UserAggregate]
+  ) {
+    self.page = page
+    self.hasNext = hasNext
+    self.repositoryIDs = repositoryIDs
+    self.repositoriesByID = repositoriesByID
+    self.ownersByID = ownersByID
+  }
+
   init() {
-    self.page = 1
-    self.hasNext = false
-    self.repositoryIDs = []
-    self.repositoriesByID = [:]
-    self.ownersByID = [:]
+    self.init(page: 1, hasNext: false, repositoryIDs: [], repositoriesByID: [:], ownersByID: [:])
   }
 
   mutating func set(response: SearchRepositoryResponse, hasNext: Bool) {
-    self.page += 1
-    self.hasNext = hasNext
-    self.repositoryIDs = response.items.map { $0.id.description }
-    self.repositoriesByID = response.items.reduce(into: [String: RepoAggregate]()) { $0[$1.id.description] = RepoAggregate(response: $1) }
-    self.ownersByID = response.items.reduce(into: [String: UserAggregate]()) { $0[$1.owner.id.description] = UserAggregate(response: $1.owner) }
+    let page = self.page + 1
+    let repositoryIDs = response.items.map { $0.id.description }
+    let repositoriesByID = response.items.reduce(into: [String: RepoAggregate]()) { $0[$1.id.description] = RepoAggregate(response: $1) }
+    let ownersByID = response.items.reduce(into: [String: UserAggregate]()) { $0[$1.owner.id.description] = UserAggregate(response: $1.owner) }
+
+    self = .init(page: page, hasNext: hasNext, repositoryIDs: repositoryIDs, repositoriesByID: repositoriesByID, ownersByID: ownersByID)
   }
 
   mutating func append(response: SearchRepositoryResponse, hasNext: Bool) {
-    self.page += 1
-    self.hasNext = hasNext
-    self.repositoryIDs.append(contentsOf: response.items.map { $0.id.description })
-    response.items.forEach {
-      self.repositoriesByID[$0.id.description] = RepoAggregate(response: $0)
-      self.ownersByID[$0.owner.id.description] = UserAggregate(response: $0.owner)
-    }
+    let page = self.page + 1
+    let repositoryIDs = self.repositoryIDs + response.items.map { $0.id.description }
+    let repositoriesByID: [String: RepoAggregate] = {
+      var repositoriesByID = self.repositoriesByID
+      response.items.forEach { repositoriesByID[$0.id.description] = RepoAggregate(response: $0) }
+      return repositoriesByID
+    }()
+
+    let ownersByID: [String: UserAggregate] = {
+      var ownersByID = self.ownersByID
+      response.items.forEach { ownersByID[$0.owner.id.description] = UserAggregate(response: $0.owner) }
+      return ownersByID
+    }()
+
+    self = .init(page: page, hasNext: hasNext, repositoryIDs: repositoryIDs, repositoriesByID: repositoriesByID, ownersByID: ownersByID)
   }
 }
--- PCSwiftUI/Aggregates/AggregateRoots/User/UserAggregateRoot.swift
+++ PCSwiftUI/Aggregates/AggregateRoots/User/UserAggregateRoot.swift
@@ -8,35 +8,43 @@
 import Foundation
 
 struct UserAggregateRoot {
-  private(set) var page: Int
-  private(set) var hasNext: Bool
-  private(set) var userIDs: [String]
-  private(set) var usersByID: [String: UserAggregate]
+  let page: Int
+  let hasNext: Bool
+  let userIDs: [String]
+  let usersByID: [String: UserAggregate]
 
   var users: [UserAggregate] {
     userIDs.compactMap { usersByID[$0] }
   }
 
+  init(page: Int, hasNext: Bool, userIDs: [String], usersByID: [String: UserAggregate]) {
+    self.page = page
+    self.hasNext = hasNext
+    self.userIDs = userIDs
+    self.usersByID = usersByID
+  }
+
   init() {
-    self.page = 1
-    self.hasNext = false
-    self.userIDs = []
-    self.usersByID = [:]
+    self.init(page: 1, hasNext: false, userIDs: [], usersByID: [:])
   }
 
   mutating func set(response: SearchUserResponse, hasNext: Bool) {
-    self.page += 1
-    self.hasNext = hasNext
-    self.userIDs = response.items.map { $0.id.description }
-    self.usersByID = response.items.reduce(into: [String: UserAggregate]()) { $0[$1.id.description] = UserAggregate(response: $1) }
+    let page = self.page + 1
+    let userIDs = response.items.map { $0.id.description }
+    let usersByID = response.items.reduce(into: [String: UserAggregate]()) { $0[$1.id.description] = UserAggregate(response: $1) }
+    self = .init(page: page, hasNext: hasNext, userIDs: userIDs, usersByID: usersByID)
   }
 
   mutating func append(response: SearchUserResponse, hasNext: Bool) {
-    self.page += 1
-    self.hasNext = hasNext
-    self.userIDs.append(contentsOf: response.items.map { $0.id.description })
-    response.items.forEach {
-      self.usersByID[$0.id.description] = UserAggregate(response: $0)
-    }
+    let page = self.page + 1
+    let userIDs = self.userIDs + response.items.map { $0.id.description }
+    let usersByID: [String: UserAggregate] = {
+      var usersByID = self.usersByID
+      response.items.forEach {
+        usersByID[$0.id.description] = UserAggregate(response: $0)
+      }
+      return usersByID
+    }()
+    self = .init(page: page, hasNext: hasNext, userIDs: userIDs, usersByID: usersByID)
   }
 }
