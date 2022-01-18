889ebe5576cbee2be92fc45775327603a87c51f5
yuoku <appledev.yuoku@gmail.com>
2022-01-18 22:06:25 +0900

    Add function to pop to the navigation root view.

--- Presentation/Views/Root/RootView.swift
+++ Presentation/Views/Root/RootView.swift
@@ -47,18 +47,19 @@ public struct RootView: View {
         print("Deeplink .repo urlString=\(urlString)")
         // Searchタブを選択
         tabIndex = 0
-        // Push遷移していた場合にPopする
-        repoListRouter.pop()
+        // 指定タブのナビゲーションスタックのルートまでPopする
+        popToRootView(tabIndex: 0)
         // 遷移アニメーションが見えるようにするためdelayをかける
         DispatchQueue.main.asyncAfter(deadline: .now() + .milliseconds(750)) {
           repoListRouter.navigateToGeneralWebView(urlString: urlString)
         }
+
       case .user(let urlString):
         print("Deeplink .user urlString=\(urlString)")
         // Userタブを選択
         tabIndex = 1
-        // Push遷移していた場合にPopする
-        userListRouter.pop()
+        // 指定タブのナビゲーションスタックのルートまでPopする
+        popToRootView(tabIndex: 1)
         // 遷移アニメーションが見えるようにするためdelayをかける
         DispatchQueue.main.asyncAfter(deadline: .now() + .milliseconds(750)) {
           userListRouter.navigateToGeneralWebView(urlString: urlString)
@@ -67,6 +68,17 @@ public struct RootView: View {
         print("Deeplink none.")
       }
     })
+  }
+
+  private func popToRootView(tabIndex: Int) {
+    guard
+      let rootViewController = UIApplication.shared.windows.filter({ $0.isKeyWindow }).first?.rootViewController,
+      let tabBarController = rootViewController.children.first as? UITabBarController,
+      let navigationController = tabBarController.viewControllers?[safe: tabIndex]?.children.first as? UINavigationController
+    else {
+      return
+    }
+    navigationController.popToRootViewController(animated: true)
   }
 }
 
